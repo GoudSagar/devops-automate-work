c57dcf77cf88227efe388bee75b61cd6dcae0937
devopscloudtrainer <119645417+devopscloudtrainer@users.noreply.github.com>
2024-09-19 08:35:17 +0530

    Create transaction.sh

---
+++ transaction.sh
@@ -0,0 +1,21 @@
+Overview of Git
+Version control system
+  * Centralized Version control system
+  * Distributed Version control system
+Architecture of Git
+Installation of Git
+Basic Git Commands
+Git Tagging
+Git Branches
+  * Overview of Branch
+  * Branching Strategy
+  * Standard Branches
+Merge and Rebase
+Cherry-Pick
+Github Account Creation
+Github Administration Part-1:
+  *  Repository creation and deletion
+  *  Branch creation and deletion
+  * Tag creation and deletion
+  * How to add/remove/update files and commit changes
+  * How to list the commits and check details of the commits?
